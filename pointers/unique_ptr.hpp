845b6b0a8e603739a41893b4ecbdb98359438edd
Daria <bdrdria@yandex.ru>
2026-09-13 18:56:25 +0300

    feat: add unique pointer for array

--- pointers/unique_ptr.hpp
+++ pointers/unique_ptr.hpp
@@ -40,3 +40,42 @@ public:
         }
     }
 };
+
+template <typename T>
+class UniquePtrArr {
+private:
+    T* ptr;
+public:
+    UniquePtrArr(T* p = nullptr) : ptr(p) {}
+    ~UniquePtrArr() { delete[] ptr; }
+
+    UniquePtrArr(const UniquePtrArr&) = delete;
+    UniquePtrArr& operator=(const UniquePtrArr&) = delete;
+
+    UniquePtrArr(UniquePtrArr&& other) noexcept : ptr(other.ptr) {
+        other.ptr = nullptr;
+    }
+    UniquePtrArr& operator=(UniquePtrArr&& other) noexcept {
+        if (this != &other) {
+            delete[] ptr;
+            ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+        return *this;
+    }
+
+    T& operator[](size_t idx) const {return ptr[idx];}
+
+    T* release() {
+        T* tmp = ptr;
+        ptr = nullptr;
+        return tmp;
+    }
+
+    void reset(T* p = nullptr) {
+        if (ptr != p) {
+            delete[] ptr;
+            ptr = p;
+        }
+    }
+};
